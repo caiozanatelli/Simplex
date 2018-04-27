83a10da772c1d4dc557e96bb1df250e6c092e5fc
Caio Zanatelli <caio.zanatelli@dcc.ufmg.br>
2018-04-27 17:45:59 -0300

    Adding class for I/O treatment

---
+++ src/IOUtils.py
@@ -0,0 +1,22 @@
+import numpy as np
+from fractions import Fraction
+
+class IOUtils:
+    
+    def read_input(self, file_dir):
+        fp = open(file_dir, 'r')
+        
+        n = int(fp.readline())
+        m = int(fp.readline())
+
+        print("Number of lines:   " + str(n)
+              + "\nNumber of columns: " + str(m))
+
+        matrix_str = fp.read()
+        matrix = np.array(eval(matrix_str)).astype('object')
+
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                matrix[i][j] = Fraction(matrix[i][j])
+
+        return matrix
